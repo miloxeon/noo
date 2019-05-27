047d8f4ff4cd2d84052e9595c5d874efe59c0972
uyouthe <melvoloskov@ya.ru>
2019-05-27 18:59:45 +0300

    rights division

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Logout from 'components/logout'
 import SecretInput from 'components/secretInput'
 
-const App = props => (
-  <div>
-    <SecretInput />
-    <Logout />
-  </div>
-)
+const Guest = props => <SecretInput />
+
+const User = props => <Logout />
+
+const App = props => props.isLoggedIn ?
+  <User { ...props } /> :
+  <Guest { ...props } />
 
 App.displayName = 'App'
 
-export default App
+export default connect(
+  state => ({
+    isLoggedIn: Boolean(state.secret),
+    secret: state.secret
+  })
+)(App)
 
-export const actions = {
-  foo: (state, payload) => state
-}
+export const actions = {}
