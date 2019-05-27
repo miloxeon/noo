9903f92b50a41f49e582bdafda0d4d2d28856fa4
uyouthe <melvoloskov@ya.ru>
2019-05-27 13:41:11 +0300

    Updates

---
+++ precache-manifest.6be05f67a2e581e5dbd6b230766dc310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30d228220468fc67c852a3bc8ef913e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb42919f47727c954b6f",
+    "url": "/static/css/2.0971e70a.chunk.css"
+  },
+  {
+    "revision": "c05a4ed4692a059c983c",
+    "url": "/static/css/main.6942973f.chunk.css"
+  },
+  {
+    "revision": "cb42919f47727c954b6f",
+    "url": "/static/js/2.4a67f958.chunk.js"
+  },
+  {
+    "revision": "c05a4ed4692a059c983c",
+    "url": "/static/js/main.9a6c2925.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
