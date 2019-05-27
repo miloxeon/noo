aee351f0990d8f505b6c7bf1f4be67714e665587
uyouthe <melvoloskov@ya.ru>
2019-05-27 23:27:53 +0300

    alternative connect

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,18 @@
+import { connect as originalConnect } from 'react-redux'
+
 export const pairs = (obj, key, value) =>
   Object.keys(obj).map(x => ({
     [key]: x,
     [value]: obj[x]
   }))
+
+export const assoc = (obj, key, value) => ({ ...obj,
+  [key]: value
+})
+
+export const connect = (mstp, mdtp) => originalConnect(mstp,
+  (dispatch, props) => mdtp(
+    (type, payload) => dispatch({ type, payload }),
+    props
+  )
+)
