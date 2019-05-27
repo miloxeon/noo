e00dd35ec02c347afc625b4dc4153b8e9e4448d5
uyouthe <melvoloskov@ya.ru>
2019-05-27 18:45:07 +0300

    secret form

--- src/components/secretInput.js
+++ src/components/secretInput.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import Pad from 'ui/pad'
 import Input from 'ui/input'
+import Button from 'ui/button'
 
 const SecretInput = props => (
-  <Input
-    type='password'
-    placeholder='keyboardcat'
-    { ...props }
-  />
+  <Pad padding='32px'>
+    <h2>
+      The secret
+    </h2>
+    <p>
+      Once you enter this, there'll be no way back.
+    </p>
+    <form>
+      <Input
+        required
+        type='password'
+        placeholder='keyboardcat'
+        { ...props }
+      />
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        margin: '20px 0'
+      }}>
+        <Button>
+          Submit
+        </Button>
+      </div>
+    </form>
+  </Pad>
+
 )
 
 SecretInput.displayName = 'SecretInput'
