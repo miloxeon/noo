a48ad1904ec7c3a7ec6c6f3e47adaa1466a10ffc
uyouthe <melvoloskov@ya.ru>
2019-05-27 20:29:30 +0300

    hash

--- src/components/newDomainInput.js
+++ src/components/newDomainInput.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { keccak512 } from 'js-sha3'
 
 import Pad from 'ui/pad'
 import Input from 'ui/input'
 import Button from 'ui/button'
 import Center from 'ui/center'
+
+const hash = (domain, secret) => keccak512(domain + secret).slice(0, 16)
 
 const NewDomainInput = props => (
   <Pad padding='32px'>
@@ -56,7 +59,9 @@ export const actions = {
     ui: { ...state.ui, newDomain }
   }),
   commitNewDomain: state => ({ ...state,
-    keys: { ...state.keys, [state.ui.newDomain]: 'supersecretpassword' },
+    keys: { ...state.keys,
+      [state.ui.newDomain]: hash(state.ui.newDomain, state.secret)
+    },
     ui: { ...state.ui, newDomain: '' }
   }),
   closeNewDomain: state => ({ ...state,
