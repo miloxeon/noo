e8d2b308ebdf4499a50d0c5cc2528eff0967a8e4
uyouthe <melvoloskov@ya.ru>
2019-05-27 21:36:23 +0300

    text

--- src/components/secretInput.js
+++ src/components/secretInput.js
@@ -14,6 +14,9 @@ const SecretInput = props => (
     <p>
       Once you enter this, there'll be no way back.
     </p>
+    <p>
+      There is no way to restore the secret either.
+    </p>
     <form onSubmit={ props.commit }>
       <Input
         autoFocus
@@ -29,6 +32,16 @@ const SecretInput = props => (
         </Button>
       </Center>
     </form>
+    <h2>How it works?</h2>
+    <p>
+      Noo generates strong passwords for the websites you use. Neither the password nor the secret are sent over the internet – the whole thing happens on your device.
+    </p>
+    <p>
+      If the same secret is given, the same domain names would render the same passwords, so this is how Noo syncs. You just enter your secret and the domain – the given password will be the same.
+    </p>
+    <p>
+      There is no way to acquire your secret from the password, so you are safe here.
+    </p>
   </Pad>
 
 )
