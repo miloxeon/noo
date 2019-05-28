6b9a072c7406557ebac3fad9036ba7fccce5a4da
uyouthe <melvoloskov@ya.ru>
2019-05-28 14:04:20 +0300

    basic transitions

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { isParsable, connect } from 'lib'
+import posed, { PoseGroup } from 'react-pose'
 
 import Logout from 'components/logout'
 import DomainList from 'components/domainList'
@@ -8,12 +9,22 @@ import NewDomainInput from 'components/newDomainInput'
 
 import Pad from 'ui/pad'
 
-const Default = props => (
-  <Fragment>
-    <DomainList />
-    <Logout />
-  </Fragment>
-)
+const AnimationWrapper = posed.div({
+  enter: {
+    x: 0,
+    opacity: 1,
+    delay: 150,
+    transition: {
+      x: { type: 'spring', stiffness: 1000, damping: 15 },
+      default: { duration: 300 }
+    }
+  },
+  exit: {
+    x: 50,
+    opacity: 0,
+    transition: { duration: 150 }
+  }
+})
 
 class App extends Component {
   static displayName = 'App'
@@ -25,9 +36,24 @@ class App extends Component {
   render () {
     return (
       <Pad>
-        { this.props.secretInputActive && <SecretInput /> }
-        { this.props.domainInputActive && <NewDomainInput /> }
-        { this.props.domainListActive && <Default />}
+        <PoseGroup>
+          { this.props.secretInputActive &&
+            <AnimationWrapper key='SecretInput'>
+              <SecretInput />
+            </AnimationWrapper>
+          }
+          { this.props.domainInputActive &&
+            <AnimationWrapper key='NewDomainInput'>
+              <NewDomainInput />
+            </AnimationWrapper>
+          }
+          { this.props.domainListActive &&
+            <AnimationWrapper key='default'>
+              <DomainList />
+              <Logout />
+            </AnimationWrapper>
+          }
+        </PoseGroup>
       </Pad>
     )
   }
