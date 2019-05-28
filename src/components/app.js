0daaa0f3cca1219567bc8443fd9ab4f3165c3530
uyouthe <melvoloskov@ya.ru>
2019-05-28 14:16:42 +0300

    fragment

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { isParsable, connect } from 'lib'
 import posed, { PoseGroup } from 'react-pose'
 
