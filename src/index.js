ef6af7e838ded8f9b6cb809a19f6538a35c8968d
uyouthe <melvoloskov@ya.ru>
2019-05-30 17:02:40 +0300

    tippy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'modern-normalize'
+import 'react-tippy/dist/tippy.css'
 import 'style.css'
 
 import App from 'components/app'
--- src/ui/copy/index.js
+++ src/ui/copy/index.js
@@ -3,6 +3,7 @@ import style from './style.module.css'
 import { copy } from './logic'
 
 import Button from 'ui/button'
+import { Tooltip } from 'react-tippy'
 
 class Copy extends Component {
 
@@ -16,13 +17,19 @@ class Copy extends Component {
   render () {
     return (
       <Fragment>
-        <Button
-          { ...this.props }
-          onClick={ this.copy }
-          data-value={ this.props.value }
+        <Tooltip
+          title='Copied!'
+          position='top'
+          trigger='click'
         >
-          { this.props.children }
-        </Button>
+          <Button
+            { ...this.props }
+            onClick={ this.copy }
+            data-value={ this.props.value }
+            >
+              { this.props.children }
+            </Button>
+        </Tooltip>
         <textarea
           ref={ this.ghost }
           className={ style.ghost }
