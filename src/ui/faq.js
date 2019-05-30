5f138b6ac4d13c94768c9842f050b15ced15b2b1
uyouthe <melvoloskov@ya.ru>
2019-05-30 21:54:37 +0300

    more faq

--- src/ui/faq.js
+++ src/ui/faq.js
@@ -26,7 +26,16 @@ export default props => (
       </a> from the password – the algorythm is the same as used in Etherium, so you are safe here.
     </p>
     <p>
-      Noo, as well as Etherium blockchain, use the <em><a href="https://keccak.team/" target="_blank" rel="noopener noreferrer">Keccak</a></em> hash function that is pretty much <a href="https://en.wikipedia.org/wiki/SHA-3" target="_blank" rel="noopener noreferrer">can't be predicted</a>. Thus, there is no way to decode your passwords without a secret that only you know.
+      Noo, as well as Etherium blockchain, use the <em><a href="https://keccak.team/" target="_blank" rel="noopener noreferrer">Keccak</a></em> hash function that is pretty much <a href='https://en.wikipedia.org/wiki/SHA-3' target='_blank' rel='noopener noreferrer'>can't be predicted</a>. Thus, there is no way to decode your passwords without a secret that only you know.
+    </p>
+    <p className='pillbox' style={{ justifyContent: 'space-between' }}>
+      <span>
+        &copy; <a href='https://miloslav.website' target='_blank' rel='noopener noreferrer'>Miloslav Voloskov</a>
+      </span>
+      <span className='pillbox'>
+        <a href='https://github.com/uyouthe/noo' target='_blank' rel='noopener noreferrer'>GitHub</a>
+        <a href='https://cdn.jsdelivr.net/gh/uyouthe/noo/LICENSE' target='_blank' rel='noopener noreferrer'>Legal</a>
+      </span>
     </p>
   </div>
 )
