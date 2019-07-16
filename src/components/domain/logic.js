99efff9da0fb45aea23c3a01707ed1292a998adb
uyouthe <melvoloskov@ya.ru>
2019-07-16 21:47:06 +0300

    animations fixed

--- src/components/domain/logic.js
+++ src/components/domain/logic.js
@@ -2,9 +2,7 @@ export const computeStyle = props => ({
   padding: '8px',
   alignItems: 'center',
   display: 'flex',
-  flexDirection: props.domainEditingActive ? 'row-reverse' : 'row',
-  animationDelay: (-1 * (0.2 + Math.random()) / 2) + 's',
-  animationDuration: (0.2 + Math.random() / 2) + 's'
+  flexDirection: props.domainEditingActive ? 'row-reverse' : 'row'
 })
 
 export const isDomain = str => /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/.test(str)
