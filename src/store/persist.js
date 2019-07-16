e72758ba61f07c097a0a47d753e4d4b1813246c6
uyouthe <melvoloskov@ya.ru>
2019-07-16 22:01:47 +0300

    hydrate merge

--- src/store/persist.js
+++ src/store/persist.js
@@ -1,12 +1,25 @@
 import initialState from 'store/initialState'
 
+const merge = (old, current) => {
+  let result = Object.assign({}, current)
+
+  Object.keys(current).forEach(key => {
+    if (old[key]) {
+      result[key] = old[key]
+    }
+  })
+
+  return result
+}
+
 export default store => next => action => {
   const result = next(action)
   const currentState = store.getState()
   const persistedState = localStorage.getItem('noo')
+  const mergedState = merge(persistedState, currentState)
 
   if ((currentState === initialState) && Boolean(persistedState)) {
-    store.dispatch({ type: 'hydrate', payload: persistedState })
+    store.dispatch({ type: 'hydrate', payload: mergedState })
   } else {
     localStorage.setItem('noo', JSON.stringify(currentState))
   }
